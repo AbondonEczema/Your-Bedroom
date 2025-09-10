ca785de11f1b50f0a68bfb344fb6c69f4788d8a7
AbondonEczema <aleyaxis@gmail.com>
2025-09-10 11:14:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// script.js
-//email thing dont change
-document.getElementById("emailForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  const emailInput = document.getElementById("email");
-  const responseMsg = document.getElementById("responseMsg");
-
-  if (emailInput.value) {
-    responseMsg.textContent = "✅ Thanks! Check your inbox for the checklist.";
-    emailInput.value = "";
-  } else {
-    responseMsg.textContent = "❌ Please enter a valid email.";
-    responseMsg.style.color = "red";
-  }
-});
 // Toggle dropdown menu
 const menuBtn = document.querySelector(".menu-btn");
 const dropdown = document.querySelector(".dropdown");
@@ -22,6 +7,7 @@ if (menuBtn && dropdown) {
     dropdown.classList.toggle("show");
   });
 }
+
 // Cookie Banner
 const cookieBanner = document.getElementById("cookieBanner");
 const acceptCookies = document.getElementById("acceptCookies");
@@ -36,8 +22,3 @@ if (acceptCookies) {
     cookieBanner.style.display = "none"; // hides immediately
   });
 }
-
-
-
-
-
